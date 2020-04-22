906ba576bfabb590c2abea71b9f86f092798584e
Yuliya Vorobeva <ui.vorobeva@outlook.com>
2020-04-23 00:59:22 +0300

    fix urls

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '../../'
-    : '/',
+  publicPath: '/site/',
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "scss",
