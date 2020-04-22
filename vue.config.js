5ed1e67b54c556c1af9bffe67d1f1cbc01a5da84
Yuliya Vorobeva <ui.vorobeva@outlook.com>
2020-04-23 01:07:24 +0300

    add deploy file

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-const path = require("path");
 module.exports = {
   publicPath: '/site/',
-  pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./styles/main.scss")]
-    }
-  }
 }
