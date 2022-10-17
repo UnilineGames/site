928d05d20ea36e03ee76245514f3ef94ceb748c5
Roman Vorobev <rv.vorobev@jet.su>
2022-10-18 00:36:08 +0300

    update paths

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const serveStatic = require('serve-static');
 const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, '/dist')));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', {root: path.join(__dirname, '/dist')});
+});
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 app.listen(port)
