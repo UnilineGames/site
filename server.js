163cc3abbe82a60b08198580a16da7674120ac1d
Yuliya Vorobeva <yunova2552@gmail.com>
2022-11-21 16:46:32 +0300

    fix deploy routing

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const serveStatic = require('serve-static');
-const path = require('path');
+// const path = require('path');
 const app = express();
-app.use(serveStatic(path.join(__dirname, '/dist')));
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', {root: path.join(__dirname, '/dist')});
-});
+app.use(serveStatic(__dirname + '/dist'));
+// app.get('/*', function (req, res) {
+//     res.sendFile('index.html', {root: path.join(__dirname, '/dist')});
+// });
 
 const port = process.env.PORT || 80;
 app.listen(port)
